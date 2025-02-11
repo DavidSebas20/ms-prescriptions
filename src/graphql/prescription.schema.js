d9df67a3804449b2538b98764e45ebb7d088328b
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-11 12:32:02 -0500

    :sparkles: feat(prescription.schema.js): Add GraphQL schema for managing prescriptions with queries and mutations

---
+++ src/graphql/prescription.schema.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+// Define the GraphQL schema for prescriptions
+module.exports = gql`
+  type Prescription {
+    prescription_id: String!
+    patient_id: String!       
+    doctor_id: String!        
+    medication: String!
+    dosage: String!
+    date_prescribed: String!
+    notes: String
+  }
+
+  type Query {
+    getPrescription(prescription_id: String!): Prescription
+    listPrescriptions: [Prescription]
+    searchPrescriptions(patient_id: String, doctor_id: String, medication: String): [Prescription] 
+  }
+
+  type Mutation {
+    addPrescription(
+      prescription_id: String!
+      patient_id: String!      
+      doctor_id: String!       
+      medication: String!
+      dosage: String!
+      notes: String
+    ): Prescription
+
+    updatePrescription(
+      prescription_id: String!
+      patient_id: String       
+      doctor_id: String        
+      medication: String
+      dosage: String
+      notes: String
+    ): Prescription
+
+    deletePrescription(prescription_id: String!): String
+  }
+`;
